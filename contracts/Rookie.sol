995b5e47e9edb52fb3d0ef30bd89f1fe7b429a7a
Selva Raman <selvaraman.sse@gmail.com>
2023-06-07 02:00:07 +0530

    get random nft id

--- contracts/Rookie.sol
+++ contracts/Rookie.sol
@@ -8,10 +8,19 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Rookie is ERC721, ERC721URIStorage, ERC721Burnable, ERC721Enumerable, Ownable {
+contract Rookie is
+    ERC721,
+    ERC721URIStorage,
+    ERC721Burnable,
+    ERC721Enumerable,
+    Ownable
+{
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    uint256 public constant MAX_LIMIT = 10;
+    uint256 public totalAssignedNfts;
+    uint256[MAX_LIMIT] nftIdList;
 
     constructor() ERC721("Rookie", "RK") {
         _tokenIdCounter.current();
@@ -20,6 +29,7 @@ contract Rookie is ERC721, ERC721URIStorage, ERC721Burnable, ERC721Enumerable, O
     function safeMint(address to, string[] memory uri) public onlyOwner {
         for (uint256 i = 0; i < uri.length; i++) {
             uint256 tokenId = _tokenIdCounter.current();
+            require(tokenId <= MAX_LIMIT, "Rookie: Exceeds Limit");
             _tokenIdCounter.increment();
             _safeMint(to, tokenId);
             _setTokenURI(tokenId, uri[i]);
@@ -35,7 +45,35 @@ contract Rookie is ERC721, ERC721URIStorage, ERC721Burnable, ERC721Enumerable, O
     }
 
     function _baseURI() internal view virtual override returns (string memory) {
-        return "https://ipfs.io/ipfs/QmYhdGDnYzb6Z82QzEf5KPUVAUH6i62nhsVnJTiRTRea9i/";
+        return
+            "https://ipfs.io/ipfs/QmYhdGDnYzb6Z82QzEf5KPUVAUH6i62nhsVnJTiRTRea9i/";
+    }
+
+    function getRandomNftId(
+        uint256 salt
+    ) private returns (uint256 randomNftId) {
+        require(
+            totalAssignedNfts < MAX_LIMIT,
+            "Rookie: No more NFTs to assign"
+        );
+        uint256 randomNumber = uint256(
+            keccak256(abi.encodePacked(block.timestamp, block.prevrandao, salt))
+        );
+        uint256 _totalAssignedNfts = totalAssignedNfts;
+        totalAssignedNfts += 1;
+        uint256 remainingNftCount = MAX_LIMIT - _totalAssignedNfts;
+        uint256 randomNftIdIndex = randomNumber % remainingNftCount;
+        if (nftIdList[randomNftIdIndex] == 0) {
+            randomNftId = randomNftIdIndex;
+        } else {
+            randomNftId = nftIdList[randomNftIdIndex];
+        }
+        if (nftIdList[remainingNftCount - 1] == 0) {
+            nftIdList[randomNftIdIndex] = remainingNftCount - 1;
+        } else {
+            nftIdList[randomNftIdIndex] = nftIdList[remainingNftCount - 1];
+            delete nftIdList[remainingNftCount - 1];
+        }
     }
 
     function tokenURI(
@@ -44,16 +82,23 @@ contract Rookie is ERC721, ERC721URIStorage, ERC721Burnable, ERC721Enumerable, O
         return super.tokenURI(tokenId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
     function supportsInterface(
         bytes4 interfaceId
-    ) public view override(ERC721, ERC721Enumerable, ERC721URIStorage) returns (bool) {
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
         return super.supportsInterface(interfaceId);
     }
 }
